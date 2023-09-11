88755b72cbef91825491c1d057dddf3e4d2c6162
Abinav115 <114220151+Abinav115@users.noreply.github.com>
2023-09-11 16:48:11 +0530

    Update main.js

--- temperature convertor website/main.js
+++ temperature convertor website/main.js
@@ -1,33 +1,34 @@
-const celsiusInput = document.querySelector('#celsius > input');
-const fahrenheitInput = document.querySelector('#fahrenheit > input');
-const kelvinInput = document.querySelector('#kelvin > input');
+const temperatureInput = document.querySelector('#temperature-input');
+const unitSelect = document.querySelector('#unit-select');
+const convertButton = document.querySelector('#convert-button');
+const resultElement = document.querySelector('#result');
 
-const roundToTwoDP = num => num.toFixed(2);
+const convertTemperature = () => {
+  const inputValue = parseFloat(temperatureInput.value);
+  if (isNaN(inputValue)) {
+    resultElement.style.display = 'none';
+    return;
+  }
 
-const convertTemperature = (input, output1, output2, conversionFunction) => {
-  input.addEventListener('input', () => {
-    const inputValue = parseFloat(input.value);
-    const [result1, result2] = conversionFunction(inputValue);
-    output1.value = roundToTwoDP(result1);
-    output2.value = roundToTwoDP(result2);
-  });
+  const unit = unitSelect.value;
+  let convertedValue, convertedUnit;
+
+  if (unit === 'celsius') {
+    // Convert to Celsius
+    convertedValue = inputValue;
+    convertedUnit = 'Celsius';
+  } else if (unit === 'fahrenheit') {
+    // Convert to Fahrenheit
+    convertedValue = (inputValue * 9/5) + 32;
+    convertedUnit = 'Fahrenheit';
+  } else if (unit === 'kelvin') {
+    // Convert to Kelvin
+    convertedValue = inputValue + 273.15;
+    convertedUnit = 'Kelvin';
+  }
+
+  resultElement.textContent = `Converted Temperature: ${convertedValue.toFixed(2)} ${convertedUnit}`;
+  resultElement.style.display = 'block';
 };
 
-const celsiusToFahrenheitAndKelvin = celsius => [
-  celsius * 1.8 + 32,
-  celsius + 273.15
-];
-
-const fahrenheitToCelsiusAndKelvin = fahrenheit => [
-  (fahrenheit - 32) * (5 / 9),
-  (fahrenheit + 459.67) * (5 / 9)
-];
-
-const kelvinToCelsiusAndFahrenheit = kelvin => [
-  kelvin - 273,
-  1.8 * (kelvin - 273) + 32
-];
-
-convertTemperature(celsiusInput, fahrenheitInput, kelvinInput, celsiusToFahrenheitAndKelvin);
-convertTemperature(fahrenheitInput, celsiusInput, kelvinInput, fahrenheitToCelsiusAndKelvin);
-convertTemperature(kelvinInput, celsiusInput, fahrenheitInput, kelvinToCelsiusAndFahrenheit);
+convertButton.addEventListener('click', convertTemperature);
